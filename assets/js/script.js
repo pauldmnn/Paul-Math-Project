615822d2d95b52658bbf4d1afcccce51ce8dfb98
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-06-17 12:52:02 +0000

    Add division code to game

--- assets/js/script.js
+++ assets/js/script.js
@@ -89,6 +89,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"]
     } else if(operator === "-"){
         return [operand1 - operand2, "subtract"] 
+    }  else if(operator === "/"){
+        return [operand1 / operand2, "division"] 
     } else{
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}.Aborting!`;
@@ -137,4 +139,12 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
     
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "/";
+
 }
