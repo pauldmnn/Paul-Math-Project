75444b1607965519a9da1b01fb6b7778611ee5ac
Paul-vasile Dominan <pauldmnn@gmail.com>
2024-06-17 11:44:06 +0000

    Add increment to score and incorrect answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -49,8 +49,10 @@ function checkAnswer() {
 
     if(isCorrect){ 
         alert("Hey! You got it right! :D")
+        incrementScore();
     } else {
         alert(`Awww.... you answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -74,11 +76,23 @@ function calculateCorrectAnswer() {
     }
 }
 
+/**
+ * Gets the correct score from the dom and increments it by 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore
 
 }
 
+/**
+ * Gets the tally of incorrect answers from the DOM and increments it by 1
+ */
 function incrementWrongAnswer() {
+
+    let oldScore = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldScore
 
 }
 
